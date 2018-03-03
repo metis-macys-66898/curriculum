59555b749bc25751a0d1d3d8a9ee39e4283427e3
metis-macys-66898 <Dennis.pong@gmail.com>
2018-03-03 15:38:32 -0800

    solves problem 6

--- curriculum/js1/6.js
+++ curriculum/js1/6.js
@@ -5,8 +5,15 @@
  * @returns {number}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (a, sum=0, divisor = a-1) => {
+  if( a <= 1 || divisor === 1) {
+    return sum;
+}
+  if (a % divisor === 0 ) {
+     sum += divisor;
+}
+  return solution (a, sum, divisor -=1 );
+
 };
 
 module.exports = {
